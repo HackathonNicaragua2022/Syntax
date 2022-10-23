6c6c6aee550ee1eeda879e7e5bd253f4754ad71a
Steven Ulloa <ulloadeifheltsteven@gmail.com>
2022-10-22 18:51:22 -0600

    Feat: Añadiendo la Estructura del Header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,6 @@
 
 <body>
     <div id="header-holder" class="main-header">
-
         <nav id="nav" class="navbar navbar-default navbar-full">
             <div class="container-fluid">
                 <div class="container container-nav">
@@ -26,7 +25,9 @@
                                     <span class="icon-bar"></span>
                                     <span class="icon-bar"></span>
                                 </button>
-
+                                <a class="navbar-brand" href="#">
+                                    <img src="images\moat-logo.png" width="100" >
+                                </a>
                             </div>
                             <div style="height: 1px;" role="main" aria-expanded="false" class="navbar-collapse collapse" id="bs">
                                 <ul class="nav navbar-nav navbar-right">
@@ -50,5 +51,34 @@
                 </div>
             </div>
         </nav>
+
+        <div id="top-content" class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div id="main-slider">
+                            <div class="slide">
+                                <div class="noti-holder">
+                                    <a href="#">
+                                        <div class="noti">
+                                            <span class="text">Dedicado a Instituciones Educativas.</span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="spacer">
+                                <div class="big-title">Todo en <span>un solo lugar,</span> <br>donde sea, cuando sea.</div>
+                                <p>Moat es el primer paso para el desarrollo de sistemas autosuficientes dedicados a las instituciones educativas, empresas y el usuario comun.</p>
+                                </div>
+
+                                <div class="btn-holder">
+                                    <a href="registrarse.html" class="ybtn ybtn-header-color">Registrarse</a><a href="#" class="ybtn ybtn-white ybtn-shadow">Acerca de</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
